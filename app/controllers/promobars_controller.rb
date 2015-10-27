488956fb6ba53d9bca3b0533f9ab485bf25f25cd
Test <Test>
2015-10-27 18:51:46 +0200

    promobar controller refactoing

--- app/controllers/promobars_controller.rb
+++ app/controllers/promobars_controller.rb
@@ -58,8 +58,8 @@ class PromobarsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_note
-      @note = Admin::Note.find(params[:id])
+    def set_promobar
+      @promobar = Promobar.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
