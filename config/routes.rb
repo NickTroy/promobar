31020d8e89d54188ecdc23c8dcd67c55b61f41b0
Test <Test>
2015-10-27 12:17:52 +0200

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :promobars
 
-  root :to => 'home#index'
+  root :to => 'promobars#index'
   mount ShopifyApp::Engine, at: '/'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
