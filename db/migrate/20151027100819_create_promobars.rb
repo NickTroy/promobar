8ffbc2c75ba756763262f13d9fe0b6f93ab16f59
Test <Test>
2015-10-27 14:22:51 +0200

    form dev

--- db/migrate/20151027100819_create_promobars.rb
+++ db/migrate/20151027100819_create_promobars.rb
@@ -5,9 +5,7 @@ class CreatePromobars < ActiveRecord::Migration
       t.string :bar_text
       t.string :bar_font
       t.integer :bar_font_size
-      t.integer :bar_font_regular
-      t.integer :bar_font_bold
-      t.integer :bar_font_italic
+      t.integer :bar_text_formatting
       t.integer :bar_tracking
       t.integer :bar_leading
       t.string :bar_text_color
