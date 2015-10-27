d17c1994c55ecaa3e0dca8bcac7b884692501e99
Test <Test>
2015-10-27 18:50:23 +0200

    promobar controller refactoing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < AuthenticatedController
   # GET /promobars
   # GET /promobars.json
   def index
-    @promobars = Promobar.all.order(:bar_type => SORT_ASC)
+    @promobars = Promobar.all.order(:bar_type)
   end
 end
