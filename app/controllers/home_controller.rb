e00f4504e4615918d7c7524a1950c41c15e40166
Test <Test>
2015-10-28 19:11:08 +0200

    view refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,12 @@ class HomeController < AuthenticatedController
   def index
     @promobars = Promobar.all
 
-    ShopifyAPI::ScriptTag.create({event: "onload", src: "https://s3.amazonaws.com/cocacola/scripttag_test.js"})
+    if response = request.env['omniauth.auth']
+      sess = ShopifyAPI::Session.new(params[:shop], response[:credentials][:token])
+      session[:shopify] = sess
+      ShopifyAPI::Base.activate_session(sess)
+      ShopifyAPI::ScriptTag.create(:event => "onload", :src => "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
+    end
+
   end
 end
