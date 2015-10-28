0842eb1243a5f1ce8d6c261872cdaf14c5ae429a
Test <Test>
2015-10-28 15:08:00 +0200

    link url validation added

--- app/models/promobar.rb
+++ app/models/promobar.rb
@@ -5,6 +5,8 @@ class Promobar < ActiveRecord::Base
             presence: true,
             uniqueness: true
 
+=begin
   validates :url_link,
             format: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+=end
 end
