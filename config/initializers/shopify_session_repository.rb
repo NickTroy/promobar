424974670ab08f524c7c833468a7d91099f1fbce
Test <Test>
2015-10-28 19:49:04 +0200

    view refactoring

--- config/initializers/shopify_session_repository.rb
+++ config/initializers/shopify_session_repository.rb
@@ -1,5 +1,6 @@
 if Rails.configuration.cache_classes
-  ShopifyApp::SessionRepository.storage = SessionStorage
+  # ShopifyApp::SessionRepository.storage = SessionStorage
+  ShopifyApp::SessionRepository.storage = 'Shop'
 else
   ActionDispatch::Reloader.to_prepare do
     # ShopifyApp::SessionRepository.storage = SessionStorage
