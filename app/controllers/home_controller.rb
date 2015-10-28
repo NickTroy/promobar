19f1bc509c235e94375f088006c8944cd1f375be
Test <Test>
2015-10-28 19:41:03 +0200

    view refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,6 @@ class HomeController < AuthenticatedController
 
     # ShopifyAPI::ScriptTag.create(:event => "onload", :src => "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
 
-
+    session = Shop.retrieve(1)
   end
 end
