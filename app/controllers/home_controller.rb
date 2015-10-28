a41429719550d3f04aafdae71c48e5ec3f927fe3
Test <Test>
2015-10-28 19:34:38 +0200

    view refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,6 @@ class HomeController < AuthenticatedController
   def index
     @promobars = Promobar.all
 
-      session = ShopifyAPI::Session.new("https://teststore-1142.myshopify.com")
-      @sess_val = session.valid?  # returns true
-
-
       ShopifyAPI::ScriptTag.create(:event => "onload", :src => "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
 
 
