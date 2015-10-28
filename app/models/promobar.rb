79c44d5e4e8913b827510c1805b67221f3d7be71
Test <Test>
2015-10-28 12:56:47 +0200

    controller refactoring redirect issues

--- app/models/promobar.rb
+++ app/models/promobar.rb
@@ -1,7 +1,7 @@
 class Promobar < ActiveRecord::Base
   has_one :button
 
-  validates :email,
+  validates :name,
             presence: true,
             uniqueness: true,
             :format => { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
