f19c136d7e897f1d5eab097f2fc681dd98015241
Somiel <gloryanthem@gmail.com>
2015-10-29 00:17:17 +0200

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,14 +4,14 @@ class HomeController < AuthenticatedController
   def index
     @promobars = Promobar.all
 
-    ShopifyAPI::Session.setup({:api_key => ShopifyApp.config.api_key, :secret => ShopifyApp.config.secret})
+    ShopifyAPI::Session.setup({:api_key => '1542338d97c3e62b5b1e0a8b3ab8c5a8', :secret => 'dd149cca1adf1ec3e7383c02ad8b1a2e'})
     session = ShopifyAPI::Session.new(params[:shop])
     scope = ["write_script_tags"]
     permission_url = session.create_permission_url(scope)
     token = session.request_token(params)
     session = ShopifyAPI::Session.new(params[:shop], token)
     ShopifyAPI::Base.activate_session(session)
-    
+
     script = ShopifyAPI::ScriptTag.new
     script.event = "onload"
     script.src = "https://js-aplenty.com/foo.js"
