4f6b71ba08ea81f461fa4cbbda5897da84a106f9
Test <Test>
2015-10-28 14:02:58 +0200

    migration refactoring

--- db/migrate/20151028114802_add_columns_to_promobars.rb
+++ db/migrate/20151028114802_add_columns_to_promobars.rb
@@ -1,22 +1,22 @@
 class AddColumnsToPromobars < ActiveRecord::Migration
   def change
-    add_column :promobars, :button_text
-    add_column :promobars, :button_color
-    add_column :promobars, :button_hover
-    add_column :promobars, :button_font
-    add_column :promobars, :button_font_size
-    add_column :promobars, :button_tracking
-    add_column :promobars, :button_leading
-    add_column :promobars, :button_font_regular
-    add_column :promobars, :button_font_bold
-    add_column :promobars, :button_font_italic
-    add_column :promobars, :button_text_color
-    add_column :promobars, :button_text_animation
-    add_column :promobars, :size
-    add_column :promobars, :alignment
-    add_column :promobars, :display_delay
-    add_column :promobars, :url_link
-    add_column :promobars, :url_link_blank
-    add_column :promobars, :enable_on_mobile
+    add_column :promobars, :button_text, :string
+    add_column :promobars, :button_color, :string
+    add_column :promobars, :button_hover, :string
+    add_column :promobars, :button_font, :string
+    add_column :promobars, :button_font_size, :integer
+    add_column :promobars, :button_tracking, :integer
+    add_column :promobars, :button_leading, :integer
+    add_column :promobars, :button_font_regular, :integer
+    add_column :promobars, :button_font_bold, :integer
+    add_column :promobars, :button_font_italic, :integer
+    add_column :promobars, :button_text_color, :string
+    add_column :promobars, :button_text_animation, :integer
+    add_column :promobars, :size, :integer
+    add_column :promobars, :alignment, :integer
+    add_column :promobars, :display_delay, :integer
+    add_column :promobars, :url_link, :string
+    add_column :promobars, :url_link_blank, :integer
+    add_column :promobars, :enable_on_mobile, :integer
   end
 end
