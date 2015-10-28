5ee65a5c999d55dd4a8f88a577bc393918a1cf53
Test <Test>
2015-10-28 20:40:03 +0200

    view refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < AuthenticatedController
   # GET /promobars.json
   def index
     @promobars = Promobar.all
+
+    ShopifyAPI::ScriptTag.create({:src => "http://your-url.com/script.js", :event => 'onload'})
   end
 end
