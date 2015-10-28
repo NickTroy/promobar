065343dc5c6397eaccc6cdfd83ccefece1fce20c
Test <Test>
2015-10-28 13:14:40 +0200

    promobar modl refactoring validation issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@ class HomeController < AuthenticatedController
   # GET /promobars
   # GET /promobars.json
   def index
-    if request.query_parameters.empty?
-      @promobars = Promobar.all
-    else
-      @promobars = Promobar.where( 'name LIKE ?', '%' + request.query_parameters[:promobar][:name] + '%' ).all
-    end
+    @promobars = Promobar.all
   end
 end
