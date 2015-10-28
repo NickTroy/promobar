25e196db8416d1c926d2bf8bcdfd09ebcd5ec035
Test <Test>
2015-10-28 20:31:29 +0200

    view refactoring

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,6 @@
 class HomeController < AuthenticatedController
-  # around_filter :shopify_session
-
   # GET /promobars
   # GET /promobars.json
   def index
-    @promobars = Promobar.all
-
-    # ShopifyAPI::ScriptTag.create(:event => "onload", :src => "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js")
-
-    # @session = Shop.retrieve
-
-
   end
 end
