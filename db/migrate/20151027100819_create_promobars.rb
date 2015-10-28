561100fb858fb61818464ed1584161b442dc7e83
Test <Test>
2015-10-28 12:15:15 +0200

    reserve commit

--- db/migrate/20151027100819_create_promobars.rb
+++ db/migrate/20151027100819_create_promobars.rb
@@ -2,18 +2,18 @@ class CreatePromobars < ActiveRecord::Migration
   def change
     create_table :promobars do |t|
       t.integer :bar_type
-      t.string :bar_text
-      t.string :bar_font
-      t.integer :bar_font_size
-      t.integer :bar_font_regular
-      t.integer :bar_font_bold
-      t.integer :bar_font_italic
-      t.integer :bar_tracking
-      t.integer :bar_leading
-      t.integer :bar_text_formatting
-      t.string :bar_text_color
-      t.string :bar_background_color
-      t.integer :bar_background_opacity
+      t.string :text
+      t.string :font
+      t.integer :font_size
+      t.integer :font_regular
+      t.integer :font_bold
+      t.integer :font_italic
+      t.integer :tracking
+      t.integer :leading
+      t.integer :text_formatting
+      t.string :text_color
+      t.string :background_color
+      t.integer :background_opacity
 
       t.timestamps
     end
