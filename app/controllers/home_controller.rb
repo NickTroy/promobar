e902571f237f69e1f0aefb001b1c531e554ac607
Somiel <gloryanthem@gmail.com>
2015-10-29 09:22:39 +0200

    initial commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,13 @@ class HomeController < AuthenticatedController
   def index
     @promobars = Promobar.all
 
-    # ShopifyAPI::Session.setup({:api_key => '1542338d97c3e62b5b1e0a8b3ab8c5a8', :secret => 'dd149cca1adf1ec3e7383c02ad8b1a2e'})
 
-    #session = ShopifyAPI::Session.new(params[:shop])
-
-    #if session.valid?
-    #  @valid = 'true'
-
-      # ShopifyAPI::Base.activate_session(session)
+=begin
       script = ShopifyAPI::ScriptTag.new
       script.event = "onload"
       script.src = "https://js-aplenty.com/foo.js"
       script.save
+=end
     # scripts = ShopifyAPI::ScriptTag.all
 
 =begin
@@ -33,18 +28,6 @@ class HomeController < AuthenticatedController
 =end
 
     @sc = ShopifyAPI::ScriptTag.first
-    #else
-    #  @valid = 'false'
-    #end
-
-=begin
-    scope = ["write_script_tags"]
-    permission_url = session.create_permission_url(scope)
-    token = session.request_token(params)
-    session = ShopifyAPI::Session.new(params[:shop], token)
-    ShopifyAPI::Base.activate_session(session)
-=end
-
 
   end
 end
