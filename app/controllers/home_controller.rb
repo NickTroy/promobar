3161d088af38d379d415df494ffb3e76d2325729
Test <Test>
2015-10-29 12:35:45 +0200

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,14 +5,17 @@ class HomeController < AuthenticatedController
     @promobars = Promobar.all
 
     scripts = ShopifyAPI::ScriptTag.all
+    @counter = scripts.count
     unless scripts.any?
       script = ShopifyAPI::ScriptTag.new
       script.event = "onload"
       script.src = script_promobars_url
       script.save
+      
+      @counter = '1'
     end
-    
-    @counter = scripts.count
+
+
     @sc = ShopifyAPI::ScriptTag.first || 'No scripts detected'
 
   end
