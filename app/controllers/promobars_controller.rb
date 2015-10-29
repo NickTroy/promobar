c0911414e4d110caffc9fd2b7ceeba7392772992
Test <Test>
2015-10-29 14:06:21 +0200

    test

--- app/controllers/promobars_controller.rb
+++ app/controllers/promobars_controller.rb
@@ -56,7 +56,7 @@ class PromobarsController < AuthenticatedController
   # GET /promobars/script
   def script
     respond_to do |format|
-      format.js   { render :script, :content_type => 'text/javascript' }
+      format.js   { render :text => 'asd', :content_type => 'text/javascript' }
       # format.html { render :nothing => true }
     end
 
