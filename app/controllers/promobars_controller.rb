20c1379edf6f9af132ae083818d9449376e8477e
Test <Test>
2015-10-29 18:48:29 +0200

    test

--- app/controllers/promobars_controller.rb
+++ app/controllers/promobars_controller.rb
@@ -56,7 +56,7 @@ class PromobarsController < AuthenticatedController
   # GET /promobars/script
   def script
     offset = rand(Promobar.count)
-    @header_promobar = Promobar.offset(offset).first
+    @header_promobar = Promobar.where(bar_type: 1).offset(offset).first
 
     respond_to do |format|
       format.js  {}
