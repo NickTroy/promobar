0c9240c1532170eb006c98b2b3057985ad3ca181
Mykyta <nicktroyanov@gmail.com>
2015-11-04 17:06:54 +0200

    redirect uri changed

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "1542338d97c3e62b5b1e0a8b3ab8c5a8"
   config.secret = "dd149cca1adf1ec3e7383c02ad8b1a2e"
-  config.redirect_uri = "https://promobar.herokuapp.com/auth/shopify/callback"
+  config.redirect_uri = "https://promotionbar.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products, write_script_tags, read_script_tags"
   config.embedded_app = true
 end
