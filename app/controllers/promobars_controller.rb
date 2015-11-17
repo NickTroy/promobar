8284b873a8f9c1338eba7fa5c0d1fc98cb795ad3
Mykyta <nicktroyanov@gmail.com>
2015-11-17 14:22:59 +0200

    extra promobar params removed

--- app/controllers/promobars_controller.rb
+++ app/controllers/promobars_controller.rb
@@ -80,10 +80,7 @@ class PromobarsController < AuthenticatedController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def promobar_params
-      params.require(:promobar).permit(:bar_type,:name, :text, :font, :font_size, :tracking, :leading, :font_bold, :font_underlined,:font_regular,
-                                       :font_italic, :text_color, :background_color, :background_opacity, :promobar_show, :button_on, :button_text,
-                                       :button_shape, :button_color, :button_hover, :button_font, :button_font_size,
-                                       :button_tracking, :button_leading, :button_font_regular, :button_font_bold, :button_font_italic, :button_font_underlined,
-                                       :button_text_color, :text_animation, :position, :alignment, :display_delay, :url_link, :url_link_blank, :enable_on_mobile)
+      params.require(:promobar).permit(:bar_type,:name, :text, :background_color, :background_opacity, :promobar_show, :button_on, :button_text,
+                                       :button_shape, :button_color, :button_hover, :text_animation, :alignment, :url_link, :url_link_blank, :enable_on_mobile)
     end
 end
