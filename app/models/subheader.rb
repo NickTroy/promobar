55401cad8b0d981cfef42f5f6ed444440eb82a80
Mykyta <nicktroyanov@gmail.com>
2015-11-18 15:00:17 +0200

    subheader text animation

--- app/models/subheader.rb
+++ app/models/subheader.rb
@@ -1,3 +1,7 @@
 class Subheader < ActiveRecord::Base
   belongs_to :promobar
+
+  def animation animation_number
+    "animated infinite #{{ 'bounceInLeft' => 1, 'fadeIn' =>  2, 'bounce' => 3 }.key(animation_number.to_i)}"
+  end
 end
