5db13178135d0a3ad1e88323b074b94810ee4a66
Mykyta <nicktroyanov@gmail.com>
2015-11-18 10:19:07 +0200

    form subheader

--- app/controllers/subheaders_controller.rb
+++ app/controllers/subheaders_controller.rb
@@ -27,7 +27,7 @@ class SubheadersController < ApplicationController
     end
 
     def subheader_params
-      params.require(:promobar).permit(:bar_type,:name, :text, :background_color, :background_opacity, :promobar_show, :button_on, :button_text,
+      params.require(:subheader).permit(:bar_type,:name, :text, :background_color, :background_opacity, :promobar_show, :button_on, :button_text,
                                        :button_shape, :button_color, :button_hover, :text_animation, :alignment, :url_link, :url_link_blank, :enable_on_mobile)
     end
 end
