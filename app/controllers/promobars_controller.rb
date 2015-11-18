e5a10e374a93c2981e69b14cbbb33daf4e219478
Mykyta <nicktroyanov@gmail.com>
2015-11-18 09:44:46 +0200

    form subheader

--- app/controllers/promobars_controller.rb
+++ app/controllers/promobars_controller.rb
@@ -10,7 +10,7 @@ class PromobarsController < AuthenticatedController
 
   # GET /promobars/edit
   def edit
-    @subheader = @promobar.subheader
+    @subheader = @promobar.subheader || @promobar.create_subheader
   end
 
   # POST /promobars
