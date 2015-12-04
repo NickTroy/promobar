c9753ac646b2a9cd9355df3717284f9fd625dfcc
nicktroy <nicktroyanov@gmail.com>
2015-12-04 10:46:27 +0000

    animate once

--- app/models/promobar.rb
+++ app/models/promobar.rb
@@ -13,7 +13,7 @@ class Promobar < ActiveRecord::Base
   has_one :subheader
 
   def animation animation_number
-    "animated infinite #{{ 'bounceInLeft' => 1, 'fadeIn' =>  2, 'bounce' => 3 }.key(animation_number.to_i)}"
+    "animated #{{ 'bounceInLeft' => 1, 'fadeIn' =>  2, 'bounce' => 3 }.key(animation_number.to_i)}"
   end
 
   def text_align align
--- app/models/subheader.rb
+++ app/models/subheader.rb
@@ -2,7 +2,7 @@ class Subheader < ActiveRecord::Base
   belongs_to :promobar
 
   def animation animation_number
-    "animated infinite #{{ 'bounceInLeft' => 1, 'fadeIn' =>  2, 'bounce' => 3 }.key(animation_number.to_i)}"
+    "animated #{{ 'bounceInLeft' => 1, 'fadeIn' =>  2, 'bounce' => 3 }.key(animation_number.to_i)}"
   end
 
   def text_align align
