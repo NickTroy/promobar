d765b79f9878a046cad4bf798fdd811d6ff6b5ba
root <root@ArborGentryServer.qgjel40opkfepkipavmrm4zvrh.bx.internal.cloudapp.net>
2016-01-11 09:51:47 +0000

    deploying on subdomen

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "1542338d97c3e62b5b1e0a8b3ab8c5a8"
   config.secret = "dd149cca1adf1ec3e7383c02ad8b1a2e"
-  config.redirect_uri = "https://apps.arborgentry.com/promobar/auth/shopify/callback"
+  config.redirect_uri = "https://promobar.arborgentry.com/auth/shopify/callback"
   config.scope = "read_orders, read_products, write_script_tags, read_script_tags"
   config.embedded_app = true
 end
